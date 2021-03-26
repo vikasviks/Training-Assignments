96ef2d4061a7c338475f36751b2405058df2ba6d
vikasviks <67867372+vikasviks@users.noreply.github.com>
2021-03-26 23:44:32 +0530

    Add files via upload
    Day7 Extension Method

---
+++ Day7.1/CalculatorLib/CalculatorLib/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorLib
+{
+    public class Calculator
+    {
+        int res;
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+
+        public int Add() {
+            return (Num1 + Num2);
+        }
+        public int Sub() {
+            return (Num1 - Num2);
+        }
+    }
+}
---
+++ Day7.1/ExtensionMethodDemo/ExtensionMethodDemo/MyExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using CalculatorLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethodDemo
+{
+    public static class MyExtensionMethods
+    {
+        public static int Mul(this Calculator c) { 
+            return (c.Num1*c.Num2);
+        }
+        public static int CountVowels(this String s) {
+            s = s.ToLower();
+            int c = 0;
+            for (int i = 0; i < s.Length; i++) {
+                if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+                    c += 1; 
+                }
+            }
+            return c;
+        }
+    }
+}
